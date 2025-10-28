9f3a03e7d66a86c328f0495398227e34482618ec
Pattaphon-sr <zenotadeo@gmail.com>
2025-10-28 21:31:23 +0700

    Add DataPopup ResultPopup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:room_reservation_system_app/features/auth/auth.dart';
+import 'package:room_reservation_system_app/features/free.dart';
 import 'features/test_screen.dart';
 import 'features/map_preview.dart';
 
@@ -13,10 +14,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+
       // home: TestScreen(),
       // home: InitialScreen(),
       // home: SignInScreen(),
       // home: SignUpScreen(),
+      // home: FloorEditorScreen(),
       home: MapPreview(),
     );
   }
